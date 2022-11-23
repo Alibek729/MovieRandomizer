b710a6e495e0779bc1f3de6d5e07af4a7f5f0787
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2022-11-24 02:00:26 +0600

    Added Movies to Randomizer

--- randomizer/ViewController.swift
+++ randomizer/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let movieDict: [String: UIImage] = [
+        "The 8th Night": #imageLiteral(resourceName: "8th night"),
+        "The Deep House": #imageLiteral(resourceName: "deepHouse"),
+        "Old": #imageLiteral(resourceName: "old"),
+        "Infinite": #imageLiteral(resourceName: "infinite"),
+        "No Time To Die": #imageLiteral(resourceName: "noTimeToDie"),
+        "Red Notice": #imageLiteral(resourceName: "redNotice"),
+        "Sweet & Sour": #imageLiteral(resourceName: "sweet&sour"),
+        "The Guilty": #imageLiteral(resourceName: "theGuilty"),
+        "Forever Reach": #imageLiteral(resourceName: "Unknown"),
+        "Venom": #imageLiteral(resourceName: "venom")
+    ]
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var myButton: UIButton!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let randomMovie = movieDict.randomElement()
+        textLabel.text = randomMovie?.key
+        imageView.image = randomMovie?.value
+        myButton.titleLabel?.text = "Press"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
+        textLabel.text = "Press button below to see a list of movies"
+        myButton.backgroundColor = .red
+        myButton.setTitle("Press", for: .normal)
     }
-
-
 }
 
